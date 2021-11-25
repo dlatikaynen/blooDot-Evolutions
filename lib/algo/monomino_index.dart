42dbab7bc0a7199bb80be5849d1c9a8ab832f3cd
dlatikay <dlatikay@outlook.com>
2021-11-25 19:37:19 +0100

    Monomino lookup

--- lib/algo/monomino_index.dart
+++ lib/algo/monomino_index.dart
@@ -1,3 +1,5 @@
+import 'package:bloo_dot_evolutions/algo/monomino_lookup.dart';
+
 class MonominoIndex {
   static const _noice = 69;
 
@@ -13,57 +15,68 @@ class MonominoIndex {
   static const cornSW = 32;
   static const cornNW = 128;
 
-  static int primeTileFrom(int tileIndex) {
-    if ([4, 16, 64].contains(tileIndex)) {
-      return 1;
+  static MonominoLookup primeTileFrom(int tileIndex) {
+    /* prime blobs */
+    if([0,1,5,7,17,21,23,29,31,85,87,95,119,127,255].contains(tileIndex)) {
+      return MonominoLookup(tileIndex);
     }
 
-    if ([20,80,65].contains(tileIndex)) {
-      return 5;
+    var rotationIndex = [4, 16, 64].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(1, numRotations: rotationIndex + 1);
     }
 
-    if ([28,112,193].contains(tileIndex)) {
-      return 7;
+    rotationIndex = [20,80,65].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(5, numRotations: rotationIndex + 1);
+    }
+
+    rotationIndex = [28,112,193].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(7, numRotations: rotationIndex + 1);
     }
 
     if (68 == tileIndex) {
-      return 17;
+      return MonominoLookup(17, numRotations: 1);
     }
 
-    if ([84,81,_noice].contains(tileIndex)) {
-      return 21;
+    rotationIndex = [84,81,_noice].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(21, numRotations: rotationIndex + 1);
     }
 
-    if ([92,113,197].contains(tileIndex)) {
-      return 23;
+    rotationIndex = [92,113,197].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(23, numRotations: rotationIndex + 1);
     }
 
-    if ([116,209,71].contains(tileIndex)) {
-      return 29;
+    rotationIndex = [116,209,71].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(29, numRotations: rotationIndex + 1);
     }
 
-    if ([124,241,199].contains(tileIndex)) {
-      return 31;
+    rotationIndex = [124,241,199].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(31, numRotations: rotationIndex + 1);
     }
 
-    if ([93,117,213].contains(tileIndex)) {
-      return 87;
+    rotationIndex = [93,117,213].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(87, numRotations: rotationIndex + 1);
     }
 
-    if ([125,245,215].contains(tileIndex)) {
-      return 95;
+    rotationIndex = [125,245,215].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(95, numRotations: rotationIndex + 1);
     }
 
     if (221 == tileIndex) {
-      return 119;
+      return MonominoLookup(119, numRotations: 1);
     }
 
-    if ([253,247,223].contains(tileIndex)) {
-      return 127;
-    }
-
-    if([0,1,5,7,17,21,23,29,31,85,87,95,119,127,255].contains(tileIndex)) {
-      return tileIndex;
+    rotationIndex = [253,247,223].indexOf(tileIndex);
+    if (rotationIndex >= 0) {
+      return MonominoLookup(127, numRotations: 1);
     }
 
     throw ArgumentError.value(
---
+++ lib/algo/monomino_lookup.dart
@@ -0,0 +1,6 @@
+class MonominoLookup {
+  final int primeIndex;
+  final int numRotations;
+
+  MonominoLookup(this.primeIndex, {this.numRotations = 0});
+}
